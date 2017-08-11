ac5b2ec9c0f58db7fff12a0825a53476a64a4548
SamTeerlinck <sammyteer@gmail.com>
2017-08-11 14:32:42 +0200

    Add controls

--- MusicPlayer/MusicPlayer.py
+++ MusicPlayer/MusicPlayer.py
@@ -38,3 +38,103 @@ def directorychooser():
 	
 	
 directorychooser()
+ 
+def updatelabel():
+	global index
+	global songname
+	v.set(listofsongs[index].replace(".mp3",""))
+	#return songname
+
+def playpause(event):
+	global index
+	if pausebutton["text"] == 'Pause':
+		pygame.mixer.music.pause()
+		pausebutton["text"] = 'Play'
+	elif pausebutton["text"] == 'Play':
+		pygame.mixer.music.unpause()
+		pausebutton["text"] = 'Pause'
+	
+def nextsong(event):
+	global index
+	if shufflevar.get() == 1:
+		numberrandom = random.randint(0,len(listofsongs)-1)
+		
+		while index == numberrandom:
+			numberrandom = random.randint(0,len(listofsongs)-1)
+
+		index = numberrandom
+		pygame.mixer.music.load(listofsongs[index])
+		pygame.mixer.music.play()
+		updatelabel()
+	else:
+		if index < len(listofsongs)-1:
+			index += 1
+			pygame.mixer.music.load(listofsongs[index])
+			pygame.mixer.music.play()
+			updatelabel()
+
+def prevsong(event):
+	global index
+	if shufflevar.get() == 1:
+		numberrandom = random.randint(0,len(listofsongs)-1)
+		
+		while index == numberrandom:
+			numberrandom = random.randint(0,len(listofsongs)-1)
+
+		index = numberrandom
+		pygame.mixer.music.load(listofsongs[index])
+		pygame.mixer.music.play()
+		updatelabel()
+	else:
+		if index > 0:
+			index -= 1
+			pygame.mixer.music.load(listofsongs[index])
+			pygame.mixer.music.play()
+			updatelabel()
+
+
+def stopsong(event):
+	pygame.mixer.music.stop()
+	v.set("")
+	#return songname
+
+
+label = Label(root,text='Selected songs:')
+label.place(x=5,y=5)
+
+listbox = Listbox(root)
+listbox.place(x=5,y=30)
+
+listofsongs.reverse()
+
+indexforloop = 1
+for items in listofsongs:
+	listbox.insert(0,indexforloop + ". " + items.replace(".mp3",""))
+	indexforloop += 1
+
+listofsongs.reverse()
+
+pausebutton = Button(root,text = 'Pause')
+pausebutton.place(x=180,y=40)
+
+nextbutton = Button(root,text = 'Next Song')
+nextbutton.place(x=180,y=70)
+
+previousbutton = Button(root,text = 'Previous Song')
+previousbutton.place(x=180,y=100)
+
+stopbutton = Button(root,text='Stop Music')
+stopbutton.place(x=180,y=130)
+
+shufflechkbox = Checkbutton(root,text = 'Shuffle', variable=shufflevar, onvalue = 1, offvalue = 0)
+shufflechkbox.place(x=180,y=160)
+
+pausebutton.bind("<Button-1>",playpause)
+nextbutton.bind("<Button-1>",nextsong)
+previousbutton.bind("<Button-1>",prevsong)
+stopbutton.bind("<Button-1>",stopsong)
+
+songlabel.place(x=40,y=185)
+
+
+root.mainloop()
